82d982ac18be3552aa176df92312baa13c181fd8
sebastianmorenoEstech <sebastian.moreno@escuelaestech.es>
2023-01-11 23:55:37 +0100

    VR-Ready

---
+++ Assets/Scripts/FoodPiece.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FoodPiece : MonoBehaviour
+{
+	public int id;
+	
+	// Awake is called when the script instance is being loaded.
+	protected void Awake()
+	{
+		UpdateID();
+	}
+	
+	public void UpdateID()
+	{
+		id=transform.GetSiblingIndex();	
+	}
+}
---
+++ Assets/Scripts/SliceTrigger.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SliceTrigger : MonoBehaviour
+{
+    [SerializeField] private Transform piece;
+    private Transform parent;
+
+    // Awake is called when the script instance is being loaded.
+    protected void Awake()
+    {
+        parent = transform.parent;
+        var bounds = GetComponent<BoxCollider>().bounds;
+    }
+
+    // OnTriggerEnter is called when the Collider other enters the trigger.
+    protected void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Blade"))
+        {
+            var oldParent = piece.parent;
+            GameObject newSlice = new GameObject();
+            Transform[] children = parent.GetComponentsInChildren<Transform>();
+            
+            piece.parent = transform.parent.parent;
+            var coll = piece.GetComponent<BoxCollider>();
+            coll.enabled = true;
+            Physics.IgnoreCollision(coll, oldParent.GetComponent<Collider>());
+
+            piece.gameObject.AddComponent<Rigidbody>();
+            Destroy(gameObject);
+        }
+    }
+}
