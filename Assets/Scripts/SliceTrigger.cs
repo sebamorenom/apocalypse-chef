89e1aaaf516ee9fd9b24bf774c4f5096a4b31b9e
sebastianmorenoEstech <sebastian.moreno@escuelaestech.es>
2023-01-12 03:23:57 +0100

    SliceTrigger testing

--- Assets/Scripts/SliceTrigger.cs
+++ Assets/Scripts/SliceTrigger.cs
@@ -1,10 +1,11 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.Search;
 using UnityEngine;
 
 public class SliceTrigger : MonoBehaviour
 {
-    [SerializeField] private Transform piece;
+    [SerializeField] private float minCuttingSpeedThreshold;
     private Transform parent;
 
     // Awake is called when the script instance is being loaded.
@@ -14,22 +15,48 @@ public class SliceTrigger : MonoBehaviour
         var bounds = GetComponent<BoxCollider>().bounds;
     }
 
-    // OnTriggerEnter is called when the Collider other enters the trigger.
-    protected void OnTriggerEnter(Collider other)
+    public void OnTriggerEnter(Collider other)
     {
-        if (other.CompareTag("Blade"))
+        if (other.CompareTag("Blade") && GetVelocitySum(other.attachedRigidbody) > minCuttingSpeedThreshold)
         {
-            var oldParent = piece.parent;
-            GameObject newSlice = new GameObject();
-            Transform[] children = parent.GetComponentsInChildren<Transform>();
-            
-            piece.parent = transform.parent.parent;
-            var coll = piece.GetComponent<BoxCollider>();
-            coll.enabled = true;
-            Physics.IgnoreCollision(coll, oldParent.GetComponent<Collider>());
+            var parent = transform.parent;
+            GameObject firstHalf = new GameObject();
+            firstHalf.name = "FirstHalf";
+            GameObject secondHalf = new GameObject();
+            secondHalf.name = "SecondHalf";
+            firstHalf.transform.parent = secondHalf.transform.parent = parent.parent;
+            Transform[] children = new Transform[parent.childCount];
+            //Fill the array of children
+            for (int i = 0; i < parent.childCount; i++)
+            {
+                children[i] = parent.GetChild(i);
+            }
 
-            piece.gameObject.AddComponent<Rigidbody>();
-            Destroy(gameObject);
+            //Get this trigger's sibling index
+            int triggerIndex = transform.GetSiblingIndex();
+            //Fill firstHalf's children
+            for (int i = 0; i < triggerIndex; i++)
+            {
+                children[i].parent = firstHalf.transform;
+            }
+
+            //Fill secondHalf's children
+            for (int i = triggerIndex + 1; i < children.Length; i++)
+            {
+                children[i].parent = secondHalf.transform;
+            }
+
+            firstHalf.AddComponent<Rigidbody>();
+            secondHalf.AddComponent<Rigidbody>();
+            Destroy(parent.gameObject);
         }
+    }
+
+    private float GetVelocitySum(Rigidbody bladeRigidbody)
+    {
+        Rigidbody parentRigidbody = transform.parent.GetComponent<Rigidbody>();
+        Vector3 bladeDirection = bladeRigidbody.transform.forward;
+        float parentSpeedBladeDirection = Vector3.Project(parentRigidbody.velocity, -bladeDirection).magnitude;
+        return bladeRigidbody.velocity.magnitude + parentSpeedBladeDirection;
     }
 }
